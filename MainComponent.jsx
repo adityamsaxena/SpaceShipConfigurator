d0a0a62643245352918899ef077c44863177bd59
adityamsaxena <79154443+adityamsaxena@users.noreply.github.com>
2025-05-26 11:57:00 +0530

    Update MainComponent.jsx

--- MainComponent.jsx
+++ MainComponent.jsx
@@ -4,178 +4,187 @@ import "./index.css";
 const Space = () => {
   const BASE_PRICE = 1000;
 
-  const colorOptions = [
-    { id: "snow", label: "Snow", cost: 0, hex: "#FFFFFF" },
-    { id: "volcano", label: "Volcano", cost: 100, hex: "#FF7A00" },
-    { id: "sky", label: "Sky", cost: 100, hex: "#6BE4FF" },
-  ];
+  const [color, setColor] = useState("snow");
+  const [power, setPower] = useState("100mw");
+  const [warp, setWarp] = useState("no");
+  const [pkg, setPkg] = useState("basic");
 
-  const powerOptions = [
-    { id: "100mw", label: "100 MW", cost: 0 },
-    { id: "150mw", label: "150 MW", cost: 200 },
-    { id: "200mw", label: "200 MW", cost: 500 },
-  ];
+ 
+  const colorCost = color === "volcano" || color === "sky" ? 100 : 0;
+  const powerCost = power === "150mw" ? 200 : power === "200mw" ? 500 : 0;
+  const warpCost = warp === "yes" ? 1000 : 0;
+  const packageCost = pkg === "sport" ? 100 : pkg === "lux" ? 500 : 0;
 
-  const warpOptions = [
-    { id: "no", label: "NO", cost: 0 },
-    { id: "yes", label: "YES", cost: 1000 },
-  ];
+  const total = BASE_PRICE + colorCost + powerCost + warpCost + packageCost;
 
-  const packageOptions = [
-    {
-      id: "basic",
-      label: "Basic",
-      cost: 0,
-      features: ["Air Conditioning", "Cloth Seats", "FM Radio"],
-    },
-    {
-      id: "sport",
-      label: "Sport",
-      cost: 100,
-      features: ["Air Conditioning", "Cloth Seats", "FM Radio", "Personal tech support"],
-    },
-    {
-      id: "lux",
-      label: "Lux",
-      cost: 500,
-      features: ["Air Conditioning", "Luxury Seats", "Chrome wheels", "Window tint", "Sub woofer"],
-    },
-  ];
-
-  const [color, setColor] = useState(colorOptions[0].id);
-  const [power, setPower] = useState(powerOptions[0].id);
-  const [warp, setWarp] = useState(warpOptions[0].id);
-  const [pkg, setPkg] = useState(packageOptions[0].id);
-
-  const selectedColorCost = colorOptions.find((c) => c.id === color)?.cost || 0;
-  const selectedPowerCost = powerOptions.find((p) => p.id === power)?.cost || 0;
-  const selectedWarpCost = warpOptions.find((w) => w.id === warp)?.cost || 0;
-  const selectedPackageCost = packageOptions.find((p) => p.id === pkg)?.cost || 0;
-
-  const totalCost = BASE_PRICE + selectedColorCost + selectedPowerCost + selectedWarpCost + selectedPackageCost;
-
-  const highlight = (current, selected) =>
-    current === selected ? { outline: "3px solid #000" } : {};
+  const getBorder = (current, selected) =>
+    current === selected ? { outline: "3px solid purple" } : {};
 
   return (
     <div className="main">
       <div className="left-panel">
         <h1>Spaceship Configurator</h1>
 
-        <div className="color">
-          <h3>Select Color:</h3>
-        </div>
+        
+        <h3>Select Color:</h3>
         <div className="flex-container">
-          {colorOptions.map(({ id, label, cost, hex }) => (
-            <div key={id} onClick={() => setColor(id)} style={{ cursor: "pointer" }}>
-              <center>
-                <div
-                  style={{
-                    width: "64px",
-                    height: "32px",
-                    backgroundColor: hex,
-                    borderRadius: "5px",
-                    ...highlight(id, color),
-                  }}
-                ></div>
-                <br />
-                <span>+{cost}€</span>
-                <br />
-                <br />
-                <span>{label}</span>
-              </center>
-            </div>
-          ))}
-        </div>
-
-        <div className="power">
-          <h3>Select Power:</h3>
-        </div>
-        <div className="flex-container-pw">
-          {powerOptions.map(({ id, label, cost }) => (
-            <div key={id} onClick={() => setPower(id)} style={{ cursor: "pointer", ...highlight(id, power) }}>
-              <center>
-                <br />
-                <span>{label}</span>
-                <br />
-                <br />
-                <span>+{cost}€</span>
-              </center>
-            </div>
-          ))}
-        </div>
-
-        <div className="warpdrive">
-          <h3>Warp Drive:</h3>
-        </div>
-        <div className="flex-container-wd">
-          {warpOptions.map(({ id, label, cost }) => (
-            <div key={id} onClick={() => setWarp(id)} style={{ cursor: "pointer", ...highlight(id, warp) }}>
-              <center>
-                <br />
-                <span>{label}</span>
-                <br />
-                <br />
-                <span>+{cost}€</span>
-              </center>
-            </div>
-          ))}
-        </div>
-
-        <div className="option">
-          <h3>Select Option Package:</h3>
-        </div>
-        <div className="flex-container-sop">
-          {packageOptions.map(({ id, label, cost, features }) => (
+          <div onClick={() => setColor("snow")} style={{ cursor: "pointer" }}>
+            <center>
             <div
-              key={id}
               style={{
-                backgroundColor: "#5A8F4F",
-                cursor: "pointer",
-                border: id === pkg ? "3px solid black" : "none",
+                width: "64px",
+                height: "32px",
+                backgroundColor: "#FFFFFF",
+                borderRadius: "5px",
+                ...getBorder("snow", color),
               }}
-              onClick={() => setPkg(id)}
-            >
-              <center>
-                <h3>{label}</h3>
-                <span>+{cost}€</span>
-                <ul>
-                  {features.map((feature, idx) => (
-                    <li key={idx}>{feature}</li>
-                  ))}
-                </ul>
-              </center>
-            </div>
-          ))}
+            />
+            <br />
+              <span>+0€</span><br /><br />
+              <span>Snow</span>
+            </center>
+          </div>
+
+          <div onClick={() => setColor("volcano")} style={{ cursor: "pointer" }}>
+            <center>
+            <div
+              style={{
+                width: "64px",
+                height: "32px",
+                backgroundColor: "#FF7A00",
+                borderRadius: "5px",
+                ...getBorder("volcano", color),
+              }}
+            /> <br />
+            
+              <span>+100€</span><br /><br />
+              <span>Volcano</span>
+            </center>
+          </div>
+
+          <div onClick={() => setColor("sky")} style={{ cursor: "pointer" }}>
+            <center>
+            <div
+              style={{
+                width: "64px",
+                height: "32px",
+                backgroundColor: "#6BE4FF",
+                borderRadius: "5px",
+                ...getBorder("sky", color),
+              }}
+            /> <br />
+            
+              <span>+100€</span><br /><br />
+              <span>Sky</span>
+            </center>
+          </div>
+        </div>
+
+        
+        <h3>Select Power:</h3>
+        <div className="flex-container-pw">
+          <div onClick={() => setPower("100mw")} style={{ cursor: "pointer", ...getBorder("100mw", power) }}>
+            <center>
+              <span>100 MW</span><br /><br />
+              <span>+0€</span>
+            </center>
+          </div>
+
+          <div onClick={() => setPower("150mw")} style={{ cursor: "pointer", ...getBorder("150mw", power) }}>
+            <center>
+              <span>150 MW</span><br /><br />
+              <span>+200€</span>
+            </center>
+          </div>
+
+          <div onClick={() => setPower("200mw")} style={{ cursor: "pointer", ...getBorder("200mw", power) }}>
+            <center>
+              <span>200 MW</span><br /><br />
+              <span>+500€</span>
+            </center>
+          </div>
+        </div>
+
+        
+        <h3>Warp Drive:</h3>
+        <div className="flex-container-wd">
+          <div onClick={() => setWarp("no")} style={{ cursor: "pointer", ...getBorder("no", warp) }}>
+            <center>
+              <span>NO</span><br /><br />
+              <span>+0€</span>
+            </center>
+          </div>
+
+          <div onClick={() => setWarp("yes")} style={{ cursor: "pointer", ...getBorder("yes", warp) }}>
+            <center>
+              <span>YES</span><br /><br />
+              <span>+1000€</span>
+            </center>
+          </div>
+        </div>
+
+      
+        <h3>Select Option Package:</h3>
+        <div className="flex-container-sop">
+          <div
+            onClick={() => setPkg("basic")}
+            style={{ backgroundColor: "#5A8F4F", cursor: "pointer", border: pkg === "basic" ? "3px solid purple" : "none" }}
+          >
+            <center>
+              <h3>Basic</h3>
+              <span>+0€</span>
+              <ul>
+                <li>Air Conditioning</li>
+                <li>Cloth Seats</li>
+                <li>FM Radio</li>
+              </ul>
+            </center>
+          </div>
+
+          <div
+            onClick={() => setPkg("sport")}
+            style={{ backgroundColor: "#5A8F4F", cursor: "pointer", border: pkg === "sport" ? "3px solid purple" : "none" }}
+          >
+            <center>
+              <h3>Sport</h3>
+              <span>+100€</span>
+              <ul>
+                <li>Air Conditioning</li>
+                <li>Cloth Seats</li>
+                <li>FM Radio</li>
+                <li>Personal tech support</li>
+              </ul>
+            </center>
+          </div>
+
+          <div
+            onClick={() => setPkg("lux")}
+            style={{ backgroundColor: "#5A8F4F", cursor: "pointer", border: pkg === "lux" ? "3px solid purple" : "none" }}
+          >
+            <center>
+              <h3>Lux</h3>
+              <span>+500€</span>
+              <ul>
+                <li>Air Conditioning</li>
+                <li>Luxury Seats</li>
+                <li>Chrome wheels</li>
+                <li>Window tint</li>
+                <li>Sub woofer</li>
+              </ul>
+            </center>
+          </div>
         </div>
       </div>
 
+  
       <div className="right-panel">
         <h3>Pricing Summary</h3>
-        <div className="price-row">
-          <span>Base price:</span>
-          <span>{BASE_PRICE}€</span>
-        </div>
-        <div className="price-row">
-          <span>Color:</span>
-          <span>+{selectedColorCost}€</span>
-        </div>
-        <div className="price-row">
-          <span>Power:</span>
-          <span>+{selectedPowerCost}€</span>
-        </div>
-        <div className="price-row">
-          <span>Warp drive:</span>
-          <span>+{selectedWarpCost}€</span>
-        </div>
-        <div className="price-row">
-          <span>Option package:</span>
-          <span>+{selectedPackageCost}€</span>
-        </div>
-        <div className="total price-row">
-          <span>Total:</span>
-          <span>{totalCost}€</span>
-        </div>
+        <div className="price-row"><span>Base price:</span><span>{BASE_PRICE}€</span></div>
+        <div className="price-row"><span>Color:</span><span>+{colorCost}€</span></div>
+        <div className="price-row"><span>Power:</span><span>+{powerCost}€</span></div>
+        <div className="price-row"><span>Warp drive:</span><span>+{warpCost}€</span></div>
+        <div className="price-row"><span>Option package:</span><span>+{packageCost}€</span></div>
+        <div className="total price-row"><span>Total:</span><span>{total}€</span></div>
       </div>
     </div>
   );
